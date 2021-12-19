4d4608c02c77d0d4b0a11d38cb40bbafc488da5d
roottool <operation.versatile.roottool@gmail.com>
2021-12-19 17:32:33 +0900

    feat: Add aliases

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,19 @@
+alias ll="exa -l -h -@ -mU --icons --git --time-style=long-iso --color=automatic --group-directories-first"
+alias l="ll -aa"
+
+# Git
+alias g=git
+
+# Docker
+alias d=docker
+alias dps='d ps'
+alias deit='d exec -it'
+alias dlogs='d logs'
+
+# Docker compose
+alias dc='d compose'
+alias dcup='dc up'
+alias dcupd='dc up -d'
+alias dcdown='dc down'
+
+alias pbcopy='xsel --clipboard --input'
