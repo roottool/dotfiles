982b6e69bdd1a86daaf139757dc92e737e2fd9b6
roottool <operation.versatile.roottool@gmail.com>
2022-05-28 21:44:20 +0900

    Format code (#14)

--- .zsh/zinit.zsh
+++ .zsh/zinit.zsh
@@ -24,4 +24,4 @@ zinit load docker/compose
 # and it constructs scripts by `cat'ing a few files). The make'' ice could also be:
 # `make"install PREFIX=$ZPFX"`, if "install" wouldn't be the only, default target.
 zinit lucid wait'0a' for \
-as"program" pick"$ZPFX/bin/git-*" src"etc/git-extras-completion.zsh" make"PREFIX=$ZPFX" tj/git-extras
+  as"program" pick"$ZPFX/bin/git-*" src"etc/git-extras-completion.zsh" make"PREFIX=$ZPFX" tj/git-extras
