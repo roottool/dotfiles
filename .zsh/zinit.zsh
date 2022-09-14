380a5562a8b9013a2e3a62e8a38e92aec7f964fb
roottool <operation.versatile.roottool@gmail.com>
2022-09-14 10:24:36 +0900

    Remove docker compose (#19)
    Update zinit.zsh

--- .zsh/zinit.zsh
+++ .zsh/zinit.zsh
@@ -12,14 +12,6 @@ zinit light-mode for \
 zinit ice from"gh-r" as"program"
 zinit light junegunn/fzf
 
-# One other binary release, it needs renaming from `docker-compose-Linux-x86_64`.
-# This is done by ice-mod `mv'{from} -> {to}'. There are multiple packages per
-# single version, for OS X, Linux and Windows - so ice-mod `bpick' is used to
-# select Linux package - in this case this is actually not needed, Zinit will
-# grep operating system name and architecture automatically when there's no `bpick'.
-zinit ice from"gh-r" as"program" mv"docker* -> docker-compose"
-zinit load docker/compose
-
 # Scripts that are built at install (there's single default make target, "install",
 # and it constructs scripts by `cat'ing a few files). The make'' ice could also be:
 # `make"install PREFIX=$ZPFX"`, if "install" wouldn't be the only, default target.
