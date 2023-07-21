c022ee4e1ae6680479fe3350619668e43a463bb4
roottool <operation.versatile.roottool@gmail.com>
2023-07-22 06:01:07 +0900

    Add some aliases (#22)

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -3,6 +3,7 @@ alias l="ll -aa"
 
 # Git
 alias g=git
+alias gpf='g push --force-with-lease --force-if-includes'
 
 # Docker
 alias d=docker
@@ -12,9 +13,13 @@ alias dlogs='d logs'
 
 # Docker compose
 alias dc='d compose'
+alias docker-compose=dc
 alias dcup='dc up'
 alias dcupd='dc up -d'
 alias dcdown='dc down'
-alias docker-compose=dc
+alias dcstart='dc start'
+alias dcstop='dc stop'
+alias dcrestart='dc restart'
+alias drminone='d rmi `dimages -aq`'
 
 alias pbcopy='xsel --clipboard --input'
