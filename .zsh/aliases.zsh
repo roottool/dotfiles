a5ae70251345a21370c4fd6166dc3e908a1fd9cd
roottool <operation.versatile.roottool@gmail.com>
2023-07-22 06:07:33 +0900

    Fix to execute alias (#23)

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -10,6 +10,8 @@ alias d=docker
 alias dps='d ps'
 alias deit='d exec -it'
 alias dlogs='d logs'
+alias dimgs='d images'
+alias drminone='d rmi `dimgs -aq`'
 
 # Docker compose
 alias dc='d compose'
@@ -20,6 +22,5 @@ alias dcdown='dc down'
 alias dcstart='dc start'
 alias dcstop='dc stop'
 alias dcrestart='dc restart'
-alias drminone='d rmi `dimages -aq`'
 
 alias pbcopy='xsel --clipboard --input'
