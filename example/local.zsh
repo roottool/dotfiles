a2edf64cb2dd56b3328c762618dc9db718601ac6
roottool <operation.versatile.roottool@gmail.com>
2025-01-02 21:29:41 +0900

    feat: Remove Homebrew commands

--- example/local.zsh
+++ example/local.zsh
@@ -1,16 +1,6 @@
 # BROWSER
-export BROWSER='Your browser execution file path'
+export BROWSER='Your browser execution file path.'
 
 # PATH
-export WINDOWS_VSCODE='Your VS Code folder path within Windows'
+export WINDOWS_VSCODE='Your VS Code folder path within Windows.'
 export PATH=$WINDOWS_VSCODE/bin:$PATH
-
-# Check update
-updateCheckCmd="brew outdated"
-if [[ -n $(eval $updateCheckCmd) ]]; then
-  # Update by Homebrew
-  brew update
-  brew upgrade
-  brew autoremove
-  brew cleanup
-fi
